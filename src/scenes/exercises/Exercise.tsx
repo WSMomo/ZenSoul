9fa58d247c19df3190acf3110c178bbc19ac9563
Momo M <momomarcoj@gmail.com>
2023-07-29 14:33:27 +0200

    finished exercises style

--- src/scenes/exercises/Exercise.tsx
+++ src/scenes/exercises/Exercise.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 type Props = {
   bgColor: string;
   sentence: string;
@@ -10,10 +12,18 @@ export function Exercise({ side, bgColor, sentence, image }: Props) {
     <>
       {/* IF EVEN = LEFT, ELSE = RIGHT */}
       {side % 2 === 0 ? (
-        <div
-          className={`w-5/6 md:w-3/5 h-32 md:h-48 rounded-r-3xl self-start ${bgColor} flex items-center min-w-min p-2`}
+        <motion.div
+          className={`w-5/6 md:w-3/5 h-32 md:h-48 rounded-r-3xl self-start ${bgColor} flex items-center min-w-min p-2 cursor-pointer hover:bg-opacity-60`}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -150 },
+            visible: { opacity: 1, x: 0 },
+          }}
         >
-          <div className="w-3/6 md:w-4/5 px-1 text-center font-semibold h-full flex items-center">
+          <div className="w-3/6 md:w-4/5 px-1 font-semibold h-full flex items-center justify-center">
             {sentence}
           </div>
           <div className="flex justify-between  items-center">
@@ -26,10 +36,18 @@ export function Exercise({ side, bgColor, sentence, image }: Props) {
             </div>
             <div className="px-2 text-2xl font-semibold">5'</div>
           </div>
-        </div>
+        </motion.div>
       ) : (
-        <div
-          className={`w-5/6 md:w-3/5 h-32 md:h-48 rounded-l-3xl self-end ${bgColor} flex items-center min-w-min p-2`}
+        <motion.div
+          className={`w-5/6 md:w-3/5 h-32 md:h-48 rounded-l-3xl self-end ${bgColor} flex items-center min-w-min p-2 hover:bg-opacity-60 cursor-pointer`}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: 150 },
+            visible: { opacity: 1, x: 0 },
+          }}
         >
           <div className="flex justify-between items-center">
             <div className="px-2 text-2xl font-semibold">5'</div>
@@ -41,10 +59,10 @@ export function Exercise({ side, bgColor, sentence, image }: Props) {
               />
             </div>
           </div>
-          <div className="w-3/6 h-full md:w-4/5 px-2 text-center font-semibold flex items-center">
+          <div className="w-3/6 h-full md:w-4/5 px-2 font-semibold flex items-center justify-center">
             {sentence}
           </div>
-        </div>
+        </motion.div>
       )}
     </>
   );
